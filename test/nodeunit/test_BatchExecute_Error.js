c379fc3085f8060b9ab492ae7a1e7d4ac5026c78
Ovidiu Guta <ovidiu.guta@arnia.ro>
2013-06-17 18:14:02 +0300

    Added Travis testing support for entire test cases suite
    The .travis.yml script now installs CUBRID and runs the whole test cases
    suite.

--- test/nodeunit/test_BatchExecute_Error.js
+++ test/nodeunit/test_BatchExecute_Error.js
@@ -19,27 +19,31 @@ exports['test_BatchExecute_Error'] = function (test) {
         test.expect(1);
       }
       Helpers.logInfo('Connected.');
-      var sqlsArr = [];
-      sqlsArr.push('drop table if exists node_test');
-      sqlsArr.push('create table node_test(id xyz)');
-      sqlsArr.push('create table node_test(id abc)');
-      CUBRIDClient.batchExecuteNoQuery(sqlsArr, function (err) {
-        if (err instanceof Array) { //Driver version in 8.4.3 and 9.1
-          test.ok(err[0].message === '-494:Semantic: before \'  xyz)\'\nxyz is not defined. create class node_test ( id xyz ) ');
-          test.ok(err[1].message === '-494:Semantic: before \'  abc)\'\nabc is not defined. create class node_test ( id abc ) ');
-        } else {
-          test.ok(err.message === '-494:Semantic: xyz is not defined. create class node_test ( id xyz ) ');
-        }
-        CUBRIDClient.close(function (err) {
-          if (err) {
-            errorHandler(err);
-          } else {
-            Helpers.logInfo('Connection closed.');
-            Helpers.logInfo('Test passed.');
-            test.done();
-          }
-        });
+	  var sqlsArr = [];
+	  sqlsArr.push('drop table if exists node_test');
+	  sqlsArr.push('create table node_test(id xyz)');
+	  sqlsArr.push('create table node_test(id abc)');
+	  CUBRIDClient.batchExecuteNoQuery(sqlsArr, function (err) {
+		if (err instanceof Array) { //Driver version in 8.4.3
+			test.ok(err[0].message === '-494:Semantic: xyz is not defined. create class node_test ( id xyz ) ');
+			test.ok(err[1].message === '-494:Semantic: abc is not defined. create class node_test ( id abc ) ');
+		} else {
+		  if (CUBRIDClient._DB_ENGINE_VER.startsWith('9.1')) {
+			test.ok(err.message === '-494:Semantic: before \'  xyz)\'\nxyz is not defined. create class node_test ( id xyz ) ');
+		  } else {
+			test.ok(err.message === '-494:Semantic: xyz is not defined. create class node_test ( id xyz ) ');
+		  }
+		}
+		CUBRIDClient.close(function (err) {
+		  if (err) {
+			errorHandler(err);
+		  } else {
+			Helpers.logInfo('Connection closed.');
+			Helpers.logInfo('Test passed.');
+			test.done();
+		}
       });
+    });
     }
   });
 };
