7965ae9bb656061d269445f020c33a9293e5d0cd
Esen Sagynov <kadishmal@gmail.com>
2016-09-11 12:42:36 +0900

    Ref #14: Indent using spaces instead of tabs.

--- src/query/Queue.js
+++ src/query/Queue.js
@@ -1,48 +1,48 @@
 module.exports = Queue;
 
 function Queue() {
-	this.items = [];
-};
+  this.items = [];
+}
 
 Queue.prototype.push = function (item) {
-	this.items.push(item);
-	this.process();
+  this.items.push(item);
+  this.process();
 };
 
 Queue.prototype.unshift = function (item) {
-	this.items.unshift(item);
-	this.process();
+  this.items.unshift(item);
+  this.process();
 };
 
 Queue.prototype.process = function () {
-	if (!this.currentItem && this.items.length) {
-		// Get the front item.
-		this.currentItem = this.items.shift();
+  if (!this.currentItem && this.items.length) {
+    // Get the front item.
+    this.currentItem = this.items.shift();
 
-		var self = this;
+    var self = this;
 
-		// Every item is a function which calls the callback
-		// we pass when done.
-		this.currentItem(function () {
-			self.currentItem = null;
+    // Every item is a function which calls the callback
+    // we pass when done.
+    this.currentItem(function () {
+      self.currentItem = null;
 
-			self.process();
-		});
-	}
+      self.process();
+    });
+  }
 };
 
 Queue.prototype.getDepth = function () {
-	return this.items.length + (this.currentItem ? 1 : 0);
+  return this.items.length + (this.currentItem ? 1 : 0);
 };
 
 Queue.prototype.isEmpty = function () {
-	return this.items.length == 0 && !this.currentItem;
+  return this.items.length == 0 && !this.currentItem;
 };
 
 Queue.prototype.empty = function () {
-	// The most efficient way to empty an array is the following
-	// as stated in http://stackoverflow.com/a/1232046/556678.
-	this.items.length = 0;
-	// Remember, that this functions will remove all pending
-	// requests, but does not terminate the running query.
+  // The most efficient way to empty an array is the following
+  // as stated in http://stackoverflow.com/a/1232046/556678.
+  this.items.length = 0;
+  // Remember, that this functions will remove all pending
+  // requests, but does not terminate the running query.
 };
